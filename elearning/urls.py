323fef9b4c1e897271654f602c91ea6106a0a5f1
Sebastian <sebastianissimus@gmail.com>
2020-04-01 09:45:28 +0200

    create, update, delete views and templates for courses

--- elearning/urls.py
+++ elearning/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'accounts/login/$', auth_views.LoginView.as_view(), name='login'),
     url(r'accounts/logout/$', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
+    url(r'^course/', include('courses.urls')),
 ]
