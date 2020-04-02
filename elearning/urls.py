7e2e2941c673f4ea58d7b22e4fa13cad2713de4b
Sebastian <sebastianissimus@gmail.com>
2020-04-02 10:35:56 +0200

    list and detail views for courses

--- elearning/urls.py
+++ elearning/urls.py
@@ -3,9 +3,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
+from courses.views import CourseListView
+
 urlpatterns = [
     url(r'accounts/login/$', auth_views.LoginView.as_view(), name='login'),
     url(r'accounts/logout/$', auth_views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
     url(r'^course/', include('courses.urls')),
+    url(r'^$', CourseListView.as_view(), name='course_list'),
 ]
