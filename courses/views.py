dfcc1962443449605c67da23ae93ab04caa9a350
Sebastian <sebastianissimus@gmail.com>
2020-04-02 09:35:05 +0200

    modules order, drag and drop functionality, minor bux fixes

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,7 @@
+from braces.views import JsonRequestResponseMixin, CsrfExemptMixin
 from django.apps import apps
 from django.forms import modelform_factory
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
@@ -129,3 +130,17 @@ class ModuleContentListView(TemplateResponseMixin, View):
 	def get(self, request, module_id):
 		module = get_object_or_404(Module, id=module_id, course__owner=request.user)
 		return self.render_to_response({'module': module})
+
+
+class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+	def post(self, request):
+		for id, order in self.request_json.items():
+			Module.objects.filter(id=id, course__owner=request.user).update(order=order)
+		return self.render_json_response({'saved': 'OK'})
+
+
+class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+	def post(self, request):
+		for id, order in self.request_json.items():
+			Content.objects.filter(id=id, module__course__owner=request.user).update(order=order)
+		return self.render_json_response({'saved': 'OK'})
