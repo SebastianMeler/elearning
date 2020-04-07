6dbe18fef0100756ca300ac357296daba1cb2c25
Sebastian <sebastianissimus@gmail.com>
2020-04-07 11:09:57 +0200

    serialization for subjects

---
+++ courses/api/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'api'
+
+urlpatterns = [
+	url(r'^subjects/$', views.SubjectListView.as_view(), name='subject_list'),
+	url(r'^subjects/(?P<pk>\d+)/$', views.SubjectDetailView.as_view(), name='subject_detail'),
+]
